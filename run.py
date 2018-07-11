231f2e286d6e0989362dff36065852f8b9120189
HellCatVN <hellcatvn@gmail.com>
2018-07-12 01:36:28 +0700

    Complete Template

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template ,request
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route("/")
-def main():
-    return "Welcome!"
-if __name__ == "__main__":
-    app.run()   
+def index():  
+    return render_template('index.html')
+
+@app.route("/hello")
+def hello():  
+    return "Hello World!"
+
+@app.route("/hello/<string:name>/")
+def hello_user(name):  
+    return render_template('hello.html', name=name)
+
+if __name__ == "__main__":  
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run()
